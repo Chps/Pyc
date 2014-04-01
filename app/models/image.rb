c93bc335e9e7c91f888dda69c3fa314d0c29732e
Daniel Silver <das2c3@mail.missouri.edu>
2014-04-01 14:53:15 -0500

    Scaffolded images

---
+++ app/models/image.rb
@@ -0,0 +1,14 @@
+class Image < ActiveRecord::Base
+  has_attached_file :content,
+    :storage => :dropbox,
+    :dropbox_credentials => {
+        app_key: ENV["DROPBOX_APP_KEY"],
+        app_secret: ENV["DROPBOX_APP_SECRET"],
+        access_token: ENV["DROPBOX_ACCESS_TOKEN"],
+        access_token_secret: ENV["DROPBOX_ACCESS_TOKEN_SECRET"],
+        user_id: ENV["DROPBOX_USER_ID"],
+        access_type: "app_folder"
+    },
+    :dropbox_visibility => 'public',
+    :dropbox_options => {}
+end
