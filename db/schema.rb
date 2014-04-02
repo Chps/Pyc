db06c9cbb1dd52ef179023141f1f86412b8aaa01
Duncan Hill <duncan.hill@live.com>
2014-04-02 08:37:01 -0500

    Performed the fixed merge and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140401194015) do
+
   create_table "images", force: true do |t|
     t.string   "caption"
     t.datetime "created_at"
