a0074688d97f7ed2d20e27584dbdd88ea6ff65a9
Stephanie Cahail <stephcahail@gmail.com>
2014-04-02 13:43:13 -0500

    added bio field to user profile with 500 char limit

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331221832) do
+ActiveRecord::Schema.define(version: 20140402180204) do
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20140331221832) do
     t.string   "name"
     t.string   "twitter"
     t.date     "birthdate"
-    t.text     "bio"
+    t.text     "bio",                    limit: 500
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
