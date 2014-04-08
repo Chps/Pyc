fc9d016faa9a093a4b37213b3797cb81de400ef2
Daniel Silver <das2c3@mail.missouri.edu>
2014-04-08 10:28:05 -0500

    Access control for images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :user_must_be_signed_in
+  helper_method :image_user_must_be_current_user
+  helper_method :image_user_is_current_user?
+  private
+   def user_must_be_signed_in
+     redirect_to new_user_session_path, alert: "You must sign in to access this page" unless current_user
+   end
+   def authenticate_user!
+    if !current_user
+      redirect_to root_url, :alert => 'You need to sign in for access to this page.'
+    end
+   end
+   def image_user_must_be_current_user
+     redirect_to root_url, :alert => 'Access denied.' unless image_user_is_current_user?
+   end
+   def image_user_is_current_user?
+     return true if not params[:id]
+     @image ||= Image.find(params[:id])
+     current_user == @image.user
+   end
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,7 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-
+  before_filter :user_must_be_signed_in, :except => [:index, :show]
+  before_filter :image_user_must_be_current_user, :except => [:index, :show]
   # GET /images
   # GET /images.json
   def index
