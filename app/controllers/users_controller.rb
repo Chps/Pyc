3fce96211c7b265dfcccc9f86c26a7410341e6d3
Nick Pierson <nkcrpn@gmail.com>
2014-04-26 13:28:18 -0500

    Clean up UsersController, user stats page, and comments rendering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,19 +11,24 @@ class UsersController < ApplicationController
 
   def statistics
     @user = User.find(params[:id])
-    views = @user.views_by_day
+    @impressions = @user.views(select: 'user_id').group(:user_id)
 
+    views = @user.views_by_day
     if !views.empty?
-      x_labels = views.keys.map { |date| date.to_date.to_s(:short) }.sort
-      values = views.values
-      y_range = [0, values.max, values.max / 5.0]
-      @view_chart = Gchart.line(
-                      size: '400x300',
-                      data: views.values,
-                      axis_with_labels: 'x,y',
-                      axis_labels: [x_labels],
-                      axis_range: [nil, y_range]
-                    )
+      @view_chart = views_chart(views.keys, views.values, '400x300')
     end
   end
+
+  private
+    def views_chart(keys, values, size)
+      x_labels = keys.map { |date| date.to_date.to_s(:short) }.sort
+      y_range = [0, values.max, values.max / 5.0]
+      Gchart.line(
+        size:             size,
+        data:             values,
+        axis_with_labels: 'x,y',
+        axis_labels:      [x_labels],
+        axis_range:       [nil, y_range]
+      )
+    end
 end
